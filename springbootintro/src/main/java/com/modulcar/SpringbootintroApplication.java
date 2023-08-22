d9e61ceb5d580e0379ce2b6de813826c7cadc394
mehmetsungur <mehmetsungur90@gmail.com>
2023-08-22 14:43:41 +0300

    update

--- springbootintro/src/main/java/com/modulcar/SpringbootintroApplication.java
+++ springbootintro/src/main/java/com/modulcar/SpringbootintroApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootintroApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootintroApplication.class, args);
 	}
-
 }
