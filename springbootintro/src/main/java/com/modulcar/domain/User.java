3f6c48b5950856ef1bbe12aa0da61e71dd8945ae
mehmetsungur <mehmetsungur90@gmail.com>
2023-08-29 16:29:48 +0300

    update

---
+++ springbootintro/src/main/java/com/modulcar/domain/User.java
@@ -0,0 +1,33 @@
+package com.modulcar.domain;
+
+import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 25, nullable = false)
+    private String firstName;
+
+    @Column(length = 25, nullable = false)
+    private String lastName;
+
+    @Column(length = 25, nullable = false, unique = true)
+    private String userName;
+
+    @Column(length = 255, nullable = false)
+    private String password;
+
+    @
+}
